82b7216abf292742e0d848ceb6ce40aa708d8631
veda <gaddamveda@gmail.com>
2017-06-19 02:24:20 -0400

    Workout App - trial commit renaming

--- src/com/vedagaddam/springsportsworkout/MyWorkoutApp.java
+++ src/com/vedagaddam/springsportsworkout/MyWorkoutApp.java
@@ -5,7 +5,7 @@ public class MyWorkoutApp {
 	public static void main(String[] args) {
 		
 		Coach theCoach = new TrackCoach();
-		System.out.println(theCoach.getDailyWorkout());
+		//System.out.println(theCoach.getDailyWorkout());
 
 	}
 
